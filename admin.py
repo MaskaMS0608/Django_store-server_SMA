c38a86def0c86bfa041f005cd215725be7379a07
MaskaMS0608 <156016026+MaskaMS0608@users.noreply.github.com>
2025-01-22 01:47:07 +0500

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from products.models import ProductCategory, Product
+
+
+admin.site.register(Product)
+admin.site.register(ProductCategory)
+
+# from store.products.models import ProductCategory, Product
